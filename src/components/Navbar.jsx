aa87a9c7cdea0d4a31b8c5ec994d150640d2881d
Fuad Hasan <fuadhasan3503@gmail.com>
2025-05-29 18:38:35 +0600

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/icon.png"; 
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  return (
+    <div>
+      <nav className="w-11/12 mx-auto flex items-center justify-between py-4 bg-white">
+        {/* Left - Logo */}
+        <div className="flex items-center space-x-2">
+          <img src={logo} alt="Logo" className="w-10 h-10 border-4 border-green-600" />
+          <Link to="/" className="text-3xl font-bold">
+            GardenGlee
+          </Link>
         </div>
-    );
+        {/* Center - Nav Links (hidden on small devices) */}
+        <div className="hidden md:flex space-x-8 text-gray-700 font-medium">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-primary font-semibold" : "hover:text-primary"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/auth/profile"
+            className={({ isActive }) =>
+              isActive ? "text-primary font-semibold" : "hover:text-primary"
+            }
+          >
+            Explore Gardeners
+          </NavLink>
+          <NavLink
+            to="/blogs"
+            className={({ isActive }) =>
+              isActive ? "text-primary font-semibold" : "hover:text-primary"
+            }
+          >
+            Browse Tips
+          </NavLink>
+          <NavLink
+            to="/blogs"
+            className={({ isActive }) =>
+              isActive ? "text-primary font-semibold" : "hover:text-primary"
+            }
+          >
+            Share a Garden Tip
+          </NavLink>
+          <NavLink
+            to="/blogs"
+            className={({ isActive }) =>
+              isActive ? "text-primary font-semibold" : "hover:text-primary"
+            }
+          >
+             My Tips
+          </NavLink>
+        </div>
+        {/* Right - User Profile and Dropdown */}
+        <div>
+            <button>Login</button>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
